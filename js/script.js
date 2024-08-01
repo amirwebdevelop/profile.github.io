965f760d82cec736369b93b7a8322424cc444db2
amirwebdevelop <amyry1020@gmail.com>
2024-08-01 18:34:33 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
-let $ = document 
-let heder = $.querySelector(".heder_top")
+"use strict"
+
+// /////////////////////////////  function get elements  ////////////////////////////////////
+
+const getId = id => document.getElementById(id)
+
+const getClass = clas => document.querySelector(clas)
+
+const getClassAll = clas => document.querySelectorAll(clas)
 
 // ///////////////////////////////////////////////  menu heder  ///////////////////////////////////////////////
 
-let hedernavlist = $.querySelector(".heder_nav_list")
-let menu = $.getElementById("menu")
+const hedernavlist = getClass(".heder_nav_list")
+const menu = getId("menu")
 let isheder = false
-menu.addEventListener("click" , function(){
-    if (isheder === false){
+menu.addEventListener("click" , () => {
+    if (!isheder){
         hedernavlist.style.left = "0"
         menu.classList.replace("fa-bars","fa-times")
         isheder = true
@@ -19,12 +26,11 @@ menu.addEventListener("click" , function(){
 })
 
 
-
 /////////////////////////////////////////////////////////  color style  /////////////////////////////////////////////////// 
 
-let btnColor = $.querySelector(".color_btn")
-let color = $.querySelector(".color")
-let coloritems = $.querySelectorAll(".color_item")
+const btnColor = getClass(".color_btn")
+const color = getClass(".color")
+const coloritems = getClassAll(".color_item")
 
 btnColor.addEventListener("click" , function(){
     color.classList.toggle("open")
@@ -35,51 +41,69 @@ coloritems.forEach(function(item){
         let style = getComputedStyle(item)
         let color = style.backgroundColor
         localStorage.setItem("color",color)
-        $.documentElement.style.setProperty("--color-khas",color)
+        document.documentElement.style.setProperty("--color-khas",color)
     })
 })
 
-// /////////////////////////////////////////////  dark and light mod  ////////////////////////////////////
+///////////////////////////////////////////////  dark and light mod  ////////////////////////////////////
 
-
-let themeBtn = $.querySelector(".theme_item")
-let loder = $.querySelector(".loder")
-let img = $.querySelectorAll(".section4_img")
-let themeIcon = $.getElementById("themeicon")
+const themeBtn = getClass(".theme_item")
+const loder = getClass(".loder")
+const img = getClassAll(".section4_img")
+const themeIcon = getId("themeicon")
 let theme = "dark"
 let isteme = false
 
 
-themeBtn.addEventListener("click" , function(){
-    if (isteme === false){
+themeBtn.addEventListener("click" , () => {
+    
+    if (!isteme){
+
         themeIcon.classList.replace("fa-sun","fa-moon")
-        $.documentElement.style.setProperty("--color-wight","#1B2430")
-        $.documentElement.style.setProperty("--color-blac","#F5F7F8")
-        $.documentElement.style.setProperty("--color-blaclight","#F5F7F8")
+
+        document.documentElement.style.setProperty("--color-wight","#1B2430")
+
+        document.documentElement.style.setProperty("--color-blac","#EEEEEE")
+
+        document.documentElement.style.setProperty("--color-blaclight","#EEEEEE")
+
         img[0].style.border = "1px solid gray"
+
         img[1].style.border = "1px solid gray"
+
         theme = "light"
+
         localStorage.setItem("theme",theme)
+
         isteme = true
+
     }else{
+
         themeIcon.classList.replace("fa-moon","fa-sun")
-        $.documentElement.style.setProperty("--color-wight","#F5F7F8")
-        $.documentElement.style.setProperty("--color-blac","#1B2430")
-        $.documentElement.style.setProperty("--color-blaclight","#2f353d")
+
+        document.documentElement.style.setProperty("--color-wight","#EEEEEE")
+
+        document.documentElement.style.setProperty("--color-blac","#26282b")
+
+        document.documentElement.style.setProperty("--color-blaclight","#2e3135")
+
         theme = "dark"
+
         localStorage.setItem("theme",theme)
+
         isteme = false
     }
 
 })
 
+/////////////////////////////////////  event load  //////////////////////////////////////////
 
-
-window.addEventListener("load",function(){
+window.addEventListener("load",() => {
     loder.style.display = "none"
 
     let color = localStorage.getItem("color")
-    $.documentElement.style.setProperty("--color-khas",color)
+
+    document.documentElement.style.setProperty("--color-khas",color)
 
 
     let mod = localStorage.getItem("theme")
@@ -87,29 +111,54 @@ window.addEventListener("load",function(){
 
     if(mod ===  "light"){
         themeIcon.classList.replace("fa-sun","fa-moon")
-        $.documentElement.style.setProperty("--color-wight","#1B2430")
-        $.documentElement.style.setProperty("--color-blac","#F3FDE8")
-        $.documentElement.style.setProperty("--color-blaclight","#F5F7F8")
+
+        document.documentElement.style.setProperty("--color-wight","#1B2430")
+
+        document.documentElement.style.setProperty("--color-blac","#EEEEEE")
+
+        document.documentElement.style.setProperty("--color-blaclight","#EEEEEE")
+
         img[0].style.border = "1px solid gray"
+
         img[1].style.border = "1px solid gray"
+
         isteme = true
+
     }else{
+
         themeIcon.classList.replace("fa-moon","fa-sun")
-        $.documentElement.style.setProperty("--color-wight","#F5F7F8")
-        $.documentElement.style.setProperty("--color-blac","#1B2430")
-        $.documentElement.style.setProperty("--color-blaclight","#2f353d")
+
+        document.documentElement.style.setProperty("--color-wight","#EEEEEE")
+
+        document.documentElement.style.setProperty("--color-blac","#26282b")
+
+        document.documentElement.style.setProperty("--color-blaclight","#2e3135")
+
         isteme = false   
+    }
+    let content1 = getClassAll(".content1_p")
+    let content2 = getClassAll(".content2_p")
+    
+    if(window.innerWidth < 992){
+        let p1 = content1[1].innerHTML
+        let p2 = content2[0].innerHTML
+        console.log(p1);
+        console.log(p2);
+        content1[1].innerHTML = p2
+        content2[0].innerHTML = p1   
     }
 })
 
 
 
-// ////////////////////////////////////////  scroll  /////////////////////////////////////////////
-let costomscroll = $.querySelector(".scrollnavar")
+//////////////////////////////////////////  scroll  /////////////////////////////////////////////
+
+const costomscroll = getClass(".scrollnavar")
+const heder = getClass(".heder_top")
 
 window.addEventListener("scroll",function(){
-    let scrollNumber = $.documentElement.scrollTop
-    let documentHight = $.body.clientHeight
+    let scrollNumber = document.documentElement.scrollTop
+    let documentHight = document.body.clientHeight
     let widowHight = window.innerHeight
 
     let scrollpresent = scrollNumber / (documentHight - widowHight)
@@ -118,44 +167,42 @@ window.addEventListener("scroll",function(){
 
     if (scrollNumber > 100){
         heder.classList.add("hederfix")
-        if(isteme){
-            // $.documentElement.style.setProperty("--color-wight2","#2f353d")
-        }
     }
     if(scrollNumber === 0){
         heder.classList.remove("hederfix")
-        // $.documentElement.style.setProperty("--color-wight2","#F5F7F8")
+
     }
 })
 
 // ////////////////////////////////////////////  persent  ///////////////////////////////////////////
 
-let container = $.querySelector(".section3_content")
+let container = getClass(".section3_content")
+
 let arraySkills = [
     {id:1 , title: "HTML" , peresent: 90  ,icon:'<i class="fab fa-html5"></i>'},
     {id:2 , title: "CSS" , peresent: 90  ,icon:'<i class="fab fa-css3-alt"></i>'},
-    {id:3 , title: "JAVA SCRIPT" , peresent: 60  ,icon:'<i class="fab fa-js-square"></i>'},
+    {id:3 , title: "JAVA SCRIPT" , peresent: 65  ,icon:'<i class="fab fa-js-square"></i>'},
     {id:4 , title: "PYTHON" , peresent: 60  ,icon:'<i class="fab fa-python"></i>'},
     {id:5 , title: "WEB DISIGN" , peresent: 70  ,icon:'<i class="fas fa-pencil-alt"></i>'},
     {id:6 , title: "ICDL" , peresent: 100  ,icon:'<i class="fas fa-laptop"></i>'},
 ]
 
 arraySkills.forEach(function(item){
-    container.insertAdjacentHTML("beforeend",'<div class="skill_1"><div class="skill_1_information"><span class="skill_1_title">'+item.title+''+item.icon+'</span><span class="skill_1_persent">'+item.peresent+'%</span></div><div class="skill_navar1"><div class="skill_navar1_color"style=" width:'+item.peresent+'%;"></div></div></div>')
+    container.insertAdjacentHTML("beforeend",`<div class="skill_1"><div class="skill_1_information"><span class="skill_1_title"> ${item.title} ${item.icon}</span><span class="skill_1_persent"> ${item.peresent}%</span></div><div class="skill_navar1"><div class="skill_navar1_color"style=" width:${item.peresent}%;"></div></div></div>`)
 
 })
 
 // //////////////////////////////////////////  btn scroll to  ////////////////////////////////////////////////
 
-let home =$.getElementById("home")
-let information =$.getElementById("aboutme") 
-let skill =$.getElementById("skill") 
-let madarek =$.getElementById("madarek") 
-let phone =$.getElementById("phone")
-let btn1 = $.querySelector(".btn1") 
-let btn2 = $.querySelector(".btn2") 
+const home = getId("home")
+const information = getId("aboutme") 
+const skill = getId("skill") 
+const madarek = getId("madarek") 
+const phone = getId("phone")
+const btn1 = getClass(".btn1") 
+const btn2 = getClass(".btn2") 
 
-home.addEventListener("click", function() {
+home.addEventListener("click", () => {
 
         window.scrollTo({
             top: 0,
@@ -163,7 +210,7 @@ home.addEventListener("click", function() {
         });
 });
 
-information.addEventListener("click", function() {
+information.addEventListener("click", () => {
     if(window.innerWidth < 768){
         window.scrollTo({
             top: 710,
@@ -177,7 +224,7 @@ information.addEventListener("click", function() {
     }
 });
 
-skill.addEventListener("click", function() {
+skill.addEventListener("click", () => {
     if(window.innerWidth < 768){
         window.scrollTo({
             top: 1500,
@@ -192,7 +239,7 @@ skill.addEventListener("click", function() {
     }
 });
 
-madarek.addEventListener("click", function() {
+madarek.addEventListener("click", () => {
     if(window.innerWidth < 768){
         window.scrollTo({
             top: 2500,
@@ -207,7 +254,7 @@ madarek.addEventListener("click", function() {
     }
 });
 
-phone.addEventListener("click", function() {
+phone.addEventListener("click", () => {
     if(window.innerWidth < 768){
         window.scrollTo({
             top: 3450,
@@ -221,7 +268,7 @@ phone.addEventListener("click", function() {
         });
     }
 });
-btn1.addEventListener('click' , function(e){
+btn1.addEventListener('click' , (e) => {
     e.preventDefault()
     if(window.innerWidth < 768){
         window.scrollTo({
@@ -235,7 +282,7 @@ btn1.addEventListener('click' , function(e){
         });
     }
 })
-btn2.addEventListener('click' , function(e){
+btn2.addEventListener('click' , (e) => {
     e.preventDefault()
     if(window.innerWidth < 768){
         window.scrollTo({
@@ -250,5 +297,8 @@ btn2.addEventListener('click' , function(e){
         });
     }
 })
+
+
+
 
 
